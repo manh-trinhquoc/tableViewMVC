715329e34090530b6c4ef032cd0b71525bed447e
trinhquocmanh1990 <trinhquocmanh1990@gmail.com>
2018-12-05 15:11:14 +0700

    Initial Commit

---
+++ tableViewMVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewMVC
+//
+//  Created by trinhquocmanh-mac 10.12 on 12/5/18.
+//  Copyright © 2018 trinhquocmanh-mac 10.12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
